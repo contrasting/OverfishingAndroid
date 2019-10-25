114f6912ce38538a2ccc1ee2059dde8287e0a94b
fredchen <fredchen1994@outlook.com>
2019-10-25 23:01:42 +0100

    Add ProgressBar to profile image loading

--- app/src/main/java/dating/overfishing/ui/main/ProfileImageAdapter.java
+++ app/src/main/java/dating/overfishing/ui/main/ProfileImageAdapter.java
@@ -1,16 +1,23 @@
 package dating.overfishing.ui.main;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.palette.graphics.Palette;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.github.florent37.glidepalette.GlidePalette;
 
 import java.util.List;
@@ -51,16 +58,29 @@ public class ProfileImageAdapter extends PagerAdapter {
         // note this returns the root view
         View layoutView = LayoutInflater.from(container.getContext()).inflate(R.layout.item_profile_image, container, false);
         ImageView img = layoutView.findViewById(R.id.profile_image);
+        ProgressBar progress = layoutView.findViewById(R.id.profile_image_progress);
 
         Glide.with(container.getContext())
                 .load(mImages.get(position))
                 .centerCrop()
                 .transition(DrawableTransitionOptions.withCrossFade())
-                .listener(GlidePalette.with(mImages.get(position))
+                .addListener(GlidePalette.with(mImages.get(position))
                         .intoCallBack(palette -> {
                             mPalettes[position] = palette;
                             if (position == 0) mPaletteListener.onPaletteLoaded(palette);
                         }))
+                .addListener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progress.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(img);
 
         container.addView(layoutView); // is this necessary? edit: yes
