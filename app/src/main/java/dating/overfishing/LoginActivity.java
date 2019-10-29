5fe8078f92bb0bc295d810afae14d77edb6217f8
fredchen <fredchen1994@outlook.com>
2019-10-29 21:25:52 +0000

    add animated background to LoginActivity

--- app/src/main/java/dating/overfishing/LoginActivity.java
+++ app/src/main/java/dating/overfishing/LoginActivity.java
@@ -1,9 +1,10 @@
 package dating.overfishing;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,8 +14,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         findViewById(R.id.login_phone_button).setOnClickListener(v -> gotoMain());
-
         findViewById(R.id.login_facebook_button).setOnClickListener(v -> gotoMain());
+
+        setAnimatedBackground();
+    }
+
+    private void setAnimatedBackground() {
+        AnimationDrawable animationDrawable = (AnimationDrawable) findViewById(R.id.login_layout).getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 
     private void gotoMain() {
